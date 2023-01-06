9bd53447b1a66f1d9cddc532d4fce75e072ea547
Gregor Wassmann <gregor.wassmann@gmail.com>
2023-01-06 22:18:02 +0100

    Svelte with @carbon/styles

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Button from "./Button.svelte";
+</script>
+
+<main>
+	<h1>Road to v1.0</h1>
+	<p>carbon-components-svelte or maybe @carbon/svelte</p>
+</main>
+
+<Button>Version 11</Button>
+<Button>Another Button</Button>
+
+<style lang="scss">
+	@use '@carbon/styles';
+</style>
---
+++ src/Button.svelte
@@ -0,0 +1,10 @@
+<button
+  class:cds--btn={true}
+  class:cds--btn--primary={true}
+>
+  <slot></slot>
+</button>
+
+<style lang="scss">
+  @use '@carbon/styles/scss/components/button' as *;
+</style>
